53edfaaea8d8ce44f7e07a468a61c588f54b1e2c
gibong <0728na@naver.com>
2021-05-07 16:56:32 +0900

    210507 programmers 나머지한점 Counter code update

--- programmers/210507_programmers_나머지한점.py
+++ programmers/210507_programmers_나머지한점.py
@@ -1,15 +1,18 @@
-# 각 축(axis)에서 한 번 등장하는 좌표 리턴 --> 시간 복잡도 문제 발생
+# 각 축(axis)에서 한 번 등장하는 좌표 리턴, Counter 활용
+from collections import Counter
+
 def solution(v):
-    x = [p[0] for p in v]
-    y = [p[1] for p in v]
+    x = Counter([p[0] for p in v]) # dict
+    y = Counter([p[1] for p in v])
+    return [Counter.most_common(x)[-1][0], Counter.most_common(y)[-1][0]]
 
-    answer = [0, 0]
-    for i in range(len([x,y])):
-        for p in [x,y][i]:
-            if [x,y][i].count(p) == 1:
-                answer[i] = p
-                break
-
-    return answer
+# XOR 연산으로 풀이
+# def solution(v):
+#     x = [p[0] for p in v]
+#     y = [p[1] for p in v]
+#
+#
+#
+#     return
 
 print(solution([[1,4],[3,4],[3,10]]))
