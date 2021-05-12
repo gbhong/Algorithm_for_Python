5b67127e5b6c59456e1a3c892c6a247c75cfbdf6
gibong <0728na@naver.com>
2021-05-12 21:34:25 +0900

    210512 leetcode buyandsellstock code update

--- leetcode/210416_LeetCode_121_BuyandSellStock.py
+++ leetcode/210416_LeetCode_121_BuyandSellStock.py
@@ -1,11 +1,17 @@
 class Solution:
-    def maxProfit(self, prices: list):
+    def maxProfit(self, prices:list):
         if len(prices) == 1:
             return 0
-        else:
-            sell_idx, sell = max(list(enumerate(prices[1:], start=1)), key=lambda x:(x[1], x[0]))
-            buy = min(prices[:sell_idx])
-            return (sell-buy) if sell > buy else 0
+        res = []
+        buy = prices[0]
+        for i in range(len(prices)-1):
+            if buy >= prices[i+1]: # do not buy at time step i
+               buy = prices[i+1]
+            else:
+                res.append(prices[i+1]-buy) # record profit
+
+        return sorted(res)[-1] if len(res) > 0 else 0
+
 
 s = Solution()
-print(s.maxProfit([3,3,5,0,0,3,1,4]))
+print(s.maxProfit([7,6,4,3,1]))
