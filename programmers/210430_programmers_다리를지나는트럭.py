959d52e3730fda0bf9ca2e60011ef56e9ee52d71
gibong <0728na@naver.com>
2021-05-23 21:25:57 +0900

    210523 programmers 다리를지나는트럭 code update

--- programmers/210430_programmers_다리를지나는트럭.py
+++ programmers/210430_programmers_다리를지나는트럭.py
@@ -1,11 +1,18 @@
-from collections import deque
-
 def solution(bridge_length, weight, truck_weights):
-    queue = deque()
-    queue.append(truck_weights[0])
+    queue = [0]*(bridge_length-1)
+    queue.append(truck_weights.pop(0))
 
-    limit = truck_weights[0]
-    for truck in truck_weights[1:]:
+    t = 1
+    total_weight = sum(queue)
+    while queue:
+        total_weight -= queue.pop(0)
+        if truck_weights:
+            if total_weight+truck_weights[0] <= weight:
+                total_weight += truck_weights[0]
+                queue.append(truck_weights.pop(0))
+            else:
+                queue.append(0)
+        t += 1
+    return t
 
-
-    return answer
+print(solution(100, 100, [10]))
