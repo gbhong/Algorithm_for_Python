749230d5f1b3e4bc0c29e16ea98bbfc571d47051
gibong <0728na@naver.com>
2021-05-27 00:54:42 +0900

    210527 programmers 타겟 넘버 recurrence relation update

--- programmers/210524_programmers_타겟넘버.py
+++ programmers/210524_programmers_타겟넘버.py
@@ -1,20 +1,31 @@
-# BFS
-from collections import deque
+# # BFS
+# from collections import deque
+#
+# def solution(numbers, target):
+#     queue = deque()
+#     queue.append((0, 0))
+#
+#     answer = 0
+#     while queue:
+#         k, idx = queue.popleft()
+#         if idx == len(numbers):
+#             if k == target:
+#                 answer += 1
+#         else:
+#             queue.append((k + numbers[idx], idx + 1))
+#             queue.append((k - numbers[idx], idx + 1))
+#
+#     return answer
 
+# 점화식을 이용한 풀이
 def solution(numbers, target):
-    queue = deque()
-    queue.append((0, 0))
-
-    answer = 0
-    while queue:
-        k, idx = queue.popleft()
-        if idx == len(numbers):
-            if k == target:
-                answer += 1
+    if numbers == []:
+        if target == 0:
+            return 1
         else:
-            queue.append((k + numbers[idx], idx + 1))
-            queue.append((k - numbers[idx], idx + 1))
-
-    return answer
+            return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) \
+               + solution(numbers[1:], target+numbers[0])
 
 print(solution([1,1,1,1,1], 3))
