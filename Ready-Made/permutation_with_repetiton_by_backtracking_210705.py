12c50313389528fae5aefbf68bcebd763f0f5b9b
gibong <0728na@naver.com>
2021-07-05 01:29:03 +0900

    code implementation: permutation with repetition by backtracking algorithm

---
+++ Ready-Made/permutation_with_repetiton_by_backtracking_210705.py
@@ -0,0 +1,27 @@
+# define function for 'permutation with repetition', by using backtracking algorithm
+
+
+def permutation_with_repeat(arr: list, depth: int, query: str):
+    global letters, results, cnt
+
+    if query in results:
+        return
+
+    if depth > 5: # 5 is length of letters
+        return
+
+    if arr not in results and depth:
+        results.append(arr)
+        cnt += 1
+
+    for i in range(5): # length of letters
+        arr_ = arr
+        arr_ += letters[i]
+        permutation_with_repeat(arr_, depth + 1, query) # for loop continues after recursive call ends
+
+
+letters = ['A', 'E', 'I', 'O', 'U']
+results = []
+cnt = 0
+permutation_with_repeat('', 0, 'AAAE')
+print(cnt)
